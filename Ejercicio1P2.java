91fbc4871d1fed21dbbe8bac9fba3df6e073789a
Andmarez <113732434+Andmarez@users.noreply.github.com>
2022-09-16 16:52:20 -0600

    Add files via upload

---
+++ Ejercicio1P2.java
@@ -0,0 +1,10 @@
+
+package ejercicio1p2;
+public class Ejercicio1P2 {
+    public static void main(String[] args) {
+        for(int i=100;i>=1;i--)
+            System.out.println(i);
+        // TODO code application logic here
+    }
+    
+}
