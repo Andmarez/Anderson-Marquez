aa87055be661559c8f1ca90c4c926928577eb895
Andmarez <113732434+Andmarez@users.noreply.github.com>
2022-09-16 16:55:29 -0600

    Add files via upload

---
+++ Ejercicio5P2.java
@@ -0,0 +1,17 @@
+
+package ejercicio5p2;
+public class Ejercicio5P2 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i=100; i<=200; i++){
+            suma = suma +i;
+            }  
+                System.out.println("La suma es: " + suma);
+    }  
+}  
+                
+            
+     
+    
+    
+
