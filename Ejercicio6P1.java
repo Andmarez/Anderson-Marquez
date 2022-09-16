6799e18292abc1207d57628966302a5044fcef53
Andmarez <113732434+Andmarez@users.noreply.github.com>
2022-09-16 16:50:52 -0600

    Add files via upload

---
+++ Ejercicio6P1.java
@@ -0,0 +1,34 @@
+
+package ejercicio6p1;
+import java.util.Scanner;
+public class Ejercicio6P1 {
+    public static void main(String[] args) {
+    double valor, descuento, recargo, total = 0;
+    String tipo_pago;
+    Scanner entrada = new Scanner(System.in);
+    System.out.print("Ingrese el valor de la compra: ");
+    valor = entrada.nextDouble();
+    System.out.print("Ingrese tipo de pago: ");
+    tipo_pago=entrada.next();
+    descuento=0.05*valor;
+    recargo=0.03*valor;
+    if (tipo_pago.equalsIgnoreCase("contado")){
+    System.out.println("El descuento es: " + descuento);
+    total= valor - descuento;
+    System.out.println("El recargo es: " + recargo);
+    total=valor + recargo;
+    System.out.println("El total es: " + total);
+    }
+    else if (tipo_pago.equalsIgnoreCase("tarjeta")){
+    System.out.println("El recargo es: " + total);    
+    }
+    else {
+    System.out.println("Metodo de pago invalido");
+    }
+    }
+ }   
+        
+    
+    
+    
+
