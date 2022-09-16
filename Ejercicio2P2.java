a9de5ba1049d15809ec1a17d46e752cc00018203
Andmarez <113732434+Andmarez@users.noreply.github.com>
2022-09-16 16:53:21 -0600

    Add files via upload

---
+++ Ejercicio2P2.java
@@ -0,0 +1,14 @@
+
+package ejercicio2p2;
+
+public class Ejercicio2P2 {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=10;i++){
+            System.out.println("********* Tabla del "+i+" *************");
+            for (int j=1; j<=10;j++){
+                System.out.print(i+"nultiplicado por "+j+" es igual a "+i*j);
+                }
+            }
+    }
+}
